24a617b95956ef3527d13c67174e13fed7fe68a0
22mackenzie <22mackenzie.butler@boiseschools.net>
2020-08-28 19:37:44 +0000

    finished my assignment

---
+++ operations.java
@@ -0,0 +1,37 @@
+public class operations{
+ public static void main (String args[]){
+//challenge 1
+String word1 = "scissors";
+int word1a = (word1.length()/2);
+String word1b = word1.substring(0, word1a);
+String word1c = word1.substring(word1a);
+String final1= word1c + word1b;
+
+String word2 = "picture";
+int word2a = (word2.length()/2);
+String word2b = word2.substring(0, word2a);
+String word2c = word2.substring(word2a);
+String final2= word2c + word2b;
+
+String word3 = "computer" ; 
+int word3a = (word3.length()/2);
+String word3b = word3.substring(0, word3a);
+String word3c = word3.substring(word3a);
+String final3= word3c + word3b;
+
+System.out.println(final1);
+
+System.out.println(final2);
+
+System.out.println(final3);
+/*challenge 2
+Mackenzie Butler 
+Computer science 
+8/28/2020*/
+System.out.println("       __");
+System.out.println("(_____()';");
+System.out.println("/,     /`");
+System.out.println("\\\\\"- -\\\\    ");
+
+}
+} 
